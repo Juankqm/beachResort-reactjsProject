e665457416a2a3850027baefc69782b35c19469a
Juan Carlos Quiros <juan.kqm96@gmail.com>
2020-07-24 17:47:58 -0600

    Added Routing & NavBar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import './App.css'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+//Components
+import NavBar from './components/Navbar/index'
+//Pages
+import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import SingleRoom from "./pages/SingleRoom";
+import ErrorPage from "./pages/ErrorPage";
 
 function App() {
   return (
-    <div className="App">
-  
-    </div>
+    <React.Fragment>
+      <Router>
+        <NavBar/>
+        <Switch>
+          <Route exact path="/"  component={Home} />
+          <Route exact path="/rooms/" component={Rooms} />
+          <Route exact path="/rooms/:slug" component={SingleRoom} />
+          <Route exact component={ErrorPage}/>
+        </Switch>
+      </Router>
+    </React.Fragment>
   );
 }
 
