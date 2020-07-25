835bc945d108910617ee6f6d7405a79ef003164d
Juan Carlos Quiros <juan.kqm96@gmail.com>
2020-07-25 00:18:52 -0600

    Fixed setInfoServices' warning

--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -1,34 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+//icons
 import { FaCocktail, FaHiking, FaShuttleVan, FaBeer } from "react-icons/fa";
+//components
 import Title from "../Title/index";
 
 export default function Services() {
-  const [infoServices, setInfoServices] = useState([
-    {
-      icon: <FaCocktail />,
-      title: "Free Cocktails",
-      info:
-        "loremp ipsum dolor sit amet consectetur adisipicing elit. Magni, corporis",
-    },
-    {
-      icon: <FaHiking />,
-      title: "Endless Hiking",
-      info:
-        "loremp ipsum dolor sit amet consectetur adisipicing elit. Magni, corporis",
-    },
-    {
-      icon: <FaShuttleVan />,
-      title: "Free Shuttle",
-      info:
-        "loremp ipsum dolor sit amet consectetur adisipicing elit. Magni, corporis",
-    },
-    {
-      icon: <FaBeer />,
-      title: "Strongets Beers",
-      info:
-        "loremp ipsum dolor sit amet consectetur adisipicing elit. Magni, corporis",
-    },
-  ]);
+  const [infoServices, setInfoServices] = useState([{}]);
+  useEffect(() => {
+    setInfoServices([
+      {
+        icon: <FaCocktail />,
+        title: "Free Cocktails",
+        info:
+          "loremp ipsum dolor sit amet consectetur adisipicing elit. Magni, corporis",
+      },
+      {
+        icon: <FaHiking />,
+        title: "Endless Hiking",
+        info:
+          "loremp ipsum dolor sit amet consectetur adisipicing elit. Magni, corporis",
+      },
+      {
+        icon: <FaShuttleVan />,
+        title: "Free Shuttle",
+        info:
+          "loremp ipsum dolor sit amet consectetur adisipicing elit. Magni, corporis",
+      },
+      {
+        icon: <FaBeer />,
+        title: "Strongets Beers",
+        info:
+          "loremp ipsum dolor sit amet consectetur adisipicing elit. Magni, corporis",
+      },
+    ]);
+  }, []);
+
   return (
     <section className="services">
       <Title title="Services" />
